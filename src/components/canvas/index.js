00107b8ff13ab8aa29f966af28cbcdf70076279f
Jonathan Portelli <jonportelli@gmail.com>
2023-07-02 00:26:25 +0100

    Update index.js

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,7 @@
 import EarthCanvas from "./Earth";
+import ArchCanvas from "./Arch";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { EarthCanvas, ArchCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
