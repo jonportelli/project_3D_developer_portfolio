640ae0b5b83a8ae8039b19884ec8fb2565ef1dbf
Jonathan Portelli <jonportelli@gmail.com>
2023-07-03 12:56:39 +0100

    fog and positioning

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF, SoftShadows, Float, CameraControls, } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
@@ -12,9 +12,11 @@ const Earth = () => {
   return (
     <mesh>
     <ambientLight intensity={0.1} />
-    <pointLight intensity={0.2} />
-    <directionalLight position={[4, 0, 0]} castShadow intensity={5} shadow-mapSize={2048} shadow-bias={-0.001}>
-    <primitive  object={earth.scene} scale={0.5} position-y={0} position-x={0} rotation-y={0} receiveShadow />
+    {/* <pointLight intensity={0.2} /> */}
+    <directionalLight position={[4, 0, 0]} castShadow intensity={3} shadow-mapSize={2048} shadow-bias={-0.001}>
+    <SoftShadows />
+   
+    <primitive  object={earth.scene} scale={0.5} position-y={-3} position-x={-2} rotation-y={0} receiveShadow />
     </directionalLight>
     </mesh>
   );
@@ -29,18 +31,23 @@ const EarthCanvas = () => {
       gl={{ preserveDrawingBuffer: false }}
       camera={{
         fov: 45,
-        near: 0.1,
-        far: 2000,
-        position: [0, 0, 45],
+        near: 0.01,
+        far: 400,
+        position: [-20, 0, 45],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          autoRotate
+          damping={0.5}
+     
+          maxSpeed={0.05}
           enableZoom={true}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
+          {/* <CameraControls makeDefault /> */}
+        {/* <color attach="background" args={["#d0d0d0"]} /> */}
+        <fog attach="fog" args={["#d0d0d0", 8, 30]} />
         <Earth />
 
         <Preload all />
