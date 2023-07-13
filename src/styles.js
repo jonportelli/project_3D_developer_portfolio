e9f5c403bd091d4aa649c7b5cb55a2c5e46fb045
Jonathan Portelli <jonportelli@gmail.com>
2023-07-13 15:20:12 +0100

    loader position and subheading 2 fixes

--- src/styles.js
+++ src/styles.js
@@ -7,7 +7,7 @@ const styles = {
     "font-black text-primary lg:text-[80px] sm:text-[60px] sm:leading-none xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
     "font-roboto text-[#dfd9ff] lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-    heroSubText:
+    heroSubText2:
     "font-roboto text-[#dfd9ff] lg:text-[24px] sm:text-[20px] xs:text-[16px] text-[12px] lg:leading-[40px]",
   sectionHeadText:
     "text-black font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
