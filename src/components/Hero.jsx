afa7217dda010b2d2a27406df3d9b2dcd277c283
Jonathan Portelli <jonportelli@gmail.com>
2023-07-13 22:51:53 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
          
          
           
-          <div class="flex flex-col space-y-7 py-[20em] sm:flex-row sm:space-y-0 sm:space-x-8 zIndex:10">
+          <div class="flex flex-col space-y-7 py-28 sm:flex-row sm:space-y-0 sm:space-x-8 zIndex:10">
                  <a href="#" class="z-10 inline-flex items-center justify-center px-4 py-2.5 bg-[linear-gradient(90deg,#A9df,#fff)] text-base font-medium text-center text-gray-700 border-[1px] border-gray-300 rounded-lg hover:bg-gray-600 focus:ring-0 focus:ring-gray-100 dark:text-indigo dark:border-gray-200 dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                     <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
                     Show Me How
